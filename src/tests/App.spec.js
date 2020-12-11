366e8ed7ded56c55ddf78772b15c1b9be6e93075
Olakunle Salami <kunsal2003@gmail.com>
2020-12-12 00:46:19 +0100

    tdd for app component

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 describe('App component', () => {
   it('loads without error', () => {
@@ -13,6 +13,14 @@ describe('App component', () => {
     wrapper = shallow(<App />);
   })
   it('should contain Router component', () => {
-    expect(wrapper.contains(Router)).toBeTruthy();
+    expect(wrapper.find(Router)).toBeTruthy();
   });
+
+  it('should contain Switch component', () => {
+    expect(wrapper.find(Switch).length).toBe(1);
+  })
+
+  it('should contain at least one Route component', () => {
+    expect(wrapper.find(Route).length).toBeGreaterThan(0);
+  })
 });
