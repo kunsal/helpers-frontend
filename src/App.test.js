7177665732f9629a6276b454854f2fc09045ef91
Olakunle Salami <kunsal2003@gmail.com>
2020-12-11 20:49:28 +0100

    installed enzyme, deleted .babelrc and wrote demo test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+import Home from './components/Home';
+
+it('renders App without crashing', () => {
+  shallow(<App />)
+});
+
+it('renders a div of class App', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('div.App').length).toEqual(1);
+})
+
+it('renders Home component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(Home).length).toEqual(1);
+})
 
 test('renders learn react link', () => {
   render(<App />);
