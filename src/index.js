6157a3a8672e5026fa91ae84f987dd4357bcd331
Olakunle Salami <kunsal2003@gmail.com>
2020-12-21 10:04:35 +0100

    added redux-persist and dev-tool-extension

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store, { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
