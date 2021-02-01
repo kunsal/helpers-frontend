cdc2fb2ee3013f7eab059cfca0a14d0708899775
Olakunle Salami <kunsal2003@gmail.com>
2021-02-01 08:54:03 +0100

    added action cable url to actioncablebase.jsx

--- src/components/ActionCableBase.jsx
+++ src/components/ActionCableBase.jsx
@@ -5,7 +5,8 @@ import actioncable from 'actioncable';
 class ActionCableBase extends React.Component {
   constructor() {
     super();
-    this.consumer = actioncable.createConsumer(`${process.env.REACT_APP_SOCKET_URL}/cable?token=${userService.token()}`)
+    // this.consumer = actioncable.createConsumer(`${process.env.REACT_APP_SOCKET_URL}/cable?token=${userService.token()}`)
+    this.consumer = actioncable.createConsumer(`ws://kunsal-helpers.herokuapp.com/cable?token=${userService.token()}`)
   }
   
   render() { 
